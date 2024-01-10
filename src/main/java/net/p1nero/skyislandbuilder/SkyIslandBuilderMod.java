c8d7746d22d5ab90626c97c77446d91e826d6179
LZY <1070402601@qq.com>
2024-01-11 03:24:38 +0800

    Initial commit

---
+++ src/main/java/net/p1nero/skyislandbuilder/SkyIslandBuilderMod.java
@@ -0,0 +1,59 @@
+package net.p1nero.skyislandbuilder;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SkyIslandBuilderMod.MODID)
+public class SkyIslandBuilderMod
+{
+    public static final String MODID = "skyislandbuilder";//SB lol
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SkyIslandBuilderMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+        }
+    }
+}
---
+++ src/main/java/net/p1nero/skyislandbuilder/items/Clearer.java
@@ -0,0 +1,52 @@
+package net.p1nero.skyislandbuilder.items;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AirBlock;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Clearer extends Item {
+
+    public Clearer(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        DFS_Clear(context.getClickedPos(),context.getLevel());
+        return super.useOn(context);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag flag) {
+        //list.add(Component.translatable("tips.clearer"));
+        list.add(Component.literal("慎重使用！！！"));
+        super.appendHoverText(itemStack, level, list, flag);
+    }
+
+    //递归清理连着的方块
+    public void DFS_Clear(BlockPos pos, Level level){
+        if(level.getBlockState(pos).getBlock() instanceof AirBlock){
+            return;
+        }
+        level.destroyBlock(pos,true);//以后可以改成在配置文件里面设置是否显示特效
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        DFS_Clear(new BlockPos(x+1,y,z),level);
+        DFS_Clear(new BlockPos(x,y+1,z),level);
+        DFS_Clear(new BlockPos(x,y,z+1),level);
+        DFS_Clear(new BlockPos(x-1,y,z),level);
+        DFS_Clear(new BlockPos(x,y-1,z),level);
+        DFS_Clear(new BlockPos(x,y,z-1),level);
+    }
+
+}
